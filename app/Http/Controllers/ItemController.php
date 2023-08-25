cefc9dbb8a24dd263616fedf3f8dfa8bcb239913
Rakib Mahmud <rakibas375@gmail.com>
2023-08-25 23:04:05 +0600

    laravel and vue install

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $items = Item::all();
+        return response()->json($items);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $item = new Item([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+        ]);
+        $item->save();
+        return response()->json($item, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
